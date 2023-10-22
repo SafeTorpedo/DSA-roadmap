00c0ff1a3407f8308f47841214a639ffbaff5bfa
Pratham Gupta <prathamgupta.pg1@gmail.com>
2023-10-22 13:40:52 +0530

    Day 58: Median of Row-Wise Sorted Matrix

---
+++ 4) Binary Search/4.3) BS on 2D Arrays/5_matrix_median.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int countSmallerThanMid(vector<int> &row, int mid)
+{
+    int l = 0, h = row.size() - 1;
+    while (l <= h)
+    {
+        int md = (l + h) >> 1;
+        if (row[md] <= mid)
+        {
+            l = md + 1;
+        }
+        else
+        {
+            h = md - 1;
+        }
+    }
+    return l;
+}
+
+int median(vector<vector<int>> &matrix, int m, int n)
+{
+    int low = 1;
+    int high = 1e9;
+    while (low <= high)
+    {
+        int mid = (low + high) >> 1;
+        int cnt = 0;
+        for (int i = 0; i < m; i++)
+        {
+            cnt += countSmallerThanMid(matrix[i], mid);
+        }
+        if (cnt <= (n * m) / 2)
+            low = mid + 1;
+        else
+            high = mid - 1;
+    }
+    return low;
+}
