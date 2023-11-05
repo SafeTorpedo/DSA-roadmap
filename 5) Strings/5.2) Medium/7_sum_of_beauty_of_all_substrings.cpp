df85973aed2b1697cc0aaad7e95e09d51b71d03e
Pratham Gupta <prathamgupta.pg1@gmail.com>
2023-11-05 14:25:16 +0530

    Day 72: Sum of Beauty of all Substrings

---
+++ 5) Strings/5.2) Medium/7_sum_of_beauty_of_all_substrings.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    int beautySum(string s)
+    {
+        int n = s.length();
+        int ans = 0;
+
+        for (int i = 0; i < n; i++)
+        {
+            unordered_map<char, int> mp;
+            for (int j = i; j < n; j++)
+            {
+                mp[s[j]]++;
+                int maxFreq = 0, minFreq = INT_MAX;
+
+                for (auto it : mp)
+                {
+                    maxFreq = max(maxFreq, it.second);
+                    minFreq = min(minFreq, it.second);
+                }
+
+                ans += (maxFreq - minFreq);
+            }
+        }
+
+        return ans;
+    }
+};
