f94b36225aab7916910470d0ad37a2ef73c6ee43
Pratham Gupta <prathamgupta.pg1@gmail.com>
2023-11-07 12:28:06 +0530

    Day 74: Introduction to Linked List

---
+++ 6) Linked List/6.1) 1D Linked List/1_introduction.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Definition of linked list
+class Node
+{
+public:
+    int data;
+    Node *next;
+    Node() : data(0), next(nullptr) {}
+    Node(int x) : data(x), next(nullptr) {}
+    Node(int x, Node *next) : data(x), next(next) {}
+};
+
+Node *constructLL(vector<int> &arr)
+{
+    Node *head = new Node(arr[0]);
+    Node *ptr = head;
+
+    for (int i = 1; i < arr.size(); i++)
+    {
+        ptr->next = new Node(arr[i]);
+        ptr = ptr->next;
+    }
+
+    ptr->next = NULL;
+    return head;
+}
