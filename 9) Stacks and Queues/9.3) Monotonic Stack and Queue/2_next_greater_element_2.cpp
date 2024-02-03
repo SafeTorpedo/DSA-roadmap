113fa5b771434bae7a579d46c2468cbea2ba3726
Pratham Gupta <prathamgupta.pg1@gmail.com>
2024-02-03 12:01:21 +0530

    Day 162: Next Greater Element 2

---
+++ 9) Stacks and Queues/9.3) Monotonic Stack and Queue/2_next_greater_element_2.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> nextGreaterElements(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> stack, ans(n, -1);
+
+        for (int i = 0; i < n * 2; ++i) {
+            while (stack.size() && nums[stack.back()] < nums[i % n]) {
+                ans[stack.back()] = nums[i % n];
+                stack.pop_back();
+            }
+            stack.push_back(i % n);
+        }
+        return ans;
+    }
+};
